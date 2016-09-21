1824a614e871d46dbeaac62dad4a93e82a3fc7d0
ssaamberkar <sandeep.amberkar18@gmail.com>
2016-09-21 22:08:33 +0100

    Bash script for submission

---
+++ msmm_rnaseq_contScore_cocor_stratified.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Job time
+#-l h_rt=96:00:00
+#Request resources
+#-l mem=12G -l rmem=4G
+#Queue
+#$ -P hidelab
+#Cores
+#$ -pe openmp 8
+#Job Name
+#-N msmm_cocor_strat
+
+module load apps/R/3.3.0
+module load compilers/gcc/5.3
+R CMD BATCH --no-save --no-restore /shared/hidelab2/user/md4zsa/Work/Amberkar_Scripts/msmm_rnaseq_contScore_cocor_stratified.R
