a9ab86141da89505659a76c8dcbcd63120b7a394
ssaamberkar <sandeep.amberkar18@gmail.com>
2016-12-12 12:48:21 +0000

    minor edits

--- msmm_rnaseq_contScore_fullDataset.sh
+++ msmm_rnaseq_contScore_fullDataset.sh
@@ -1,12 +1,14 @@
 #!/bin/bash 
 #Job time 
-#-l h_rt=96:00:00 
+#$-l h_rt=96:00:00 
 #Request resources 
-#-l mem=8G -l rmem=8G 
+#$-l mem=8G -l rmem=8G 
 #Cores 
 #$ -pe openmp 8
+#Priority Queue
+#$-P hidelab
 #Job Name 
-#-N MSMM_FDS
+#$-N MSMM_FDS
 module load apps/R/3.3.1
 module load compilers/gcc/5.3 
 R CMD BATCH --no-save --no-restore /shared/hidelab2/user/md4zsa/Work/Amberkar_Scripts/msmm_rnaseq_contScore_fullDataset.R
