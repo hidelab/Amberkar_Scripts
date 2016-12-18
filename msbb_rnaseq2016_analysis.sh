feab9150573336117b352dbaa635306c5f164417
ssaamberkar <sandeep.amberkar18@gmail.com>
2016-12-18 20:35:47 +0000

    plq assoc. genes shell script

--- msbb_rnaseq2016_analysis.sh
+++ msbb_rnaseq2016_analysis.sh
@@ -2,9 +2,9 @@
 #Job time 
 #$-l h_rt=96:00:00 
 #Request resources 
-#$-l mem=12G -l rmem=8G 
+#$-l mem=12G -l rmem=12G 
 #Cores 
-#$ -pe openmp 12
+#$ -pe openmp 8
 #Job Name 
 #$-N MSBB2016_PLQ
 module load apps/R/3.3.1
