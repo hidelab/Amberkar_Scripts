d62fce48323b98b741c73969883c4c2d301de377
ssaamberkar <sandeep.amberkar18@gmail.com>
2017-01-19 15:04:37 +0000

    OA rewire script

--- OA_AnalyseRewire.R
+++ OA_AnalyseRewire.R
@@ -1,5 +1,6 @@
 library(igraph)
 library(org.Hs.eg.db)
+library(GOSemSim)
 
 OA_analysis=readRDS("~/Work/Collaborations/OA_EleZeggini/OA_D_vs_C_cpm_corr_change_allResults.RDS")
 OA_DCN=vector(mode = "list",length = 2)
@@ -36,4 +37,72 @@ OA_DCN.rewired_cluster2$NegCoexp_Loss=OA_DCN.rewired$NegCorr$Genes[which(OA_DCN.
 
 write.table(OA_DCN.rewired$PosCorr,"OA_DCN.rewired_PosCorr_Genes.txt",sep = "\t",col.names = T,row.names = F,quote=F)
 write.table(OA_DCN.rewired$NegCorr,"OA_DCN.rewired_NegCorr_Genes.txt",sep = "\t",col.names = T,row.names = F,quote=F)
-ge
+
+
+pos_weight=seq(1.25,1.75,by=0.05)
+neg_weight=seq(0.25,0.75,by=0.05)
+poscoexp_pval_vector=vector(mode = 'list',length=length(pos_weight))
+negcoexp_pval_vector=vector(mode = 'list',length=length(pos_weight))
+names(poscoexp_pval_vector)=pos_weight
+names(negcoexp_pval_vector)=neg_weight
+for(i in 1:length(pos_weight)){
+  poscoexp_pval_vector[[i]]=vector(mode = 'list',length=6)
+  names(poscoexp_pval_vector[[i]])=c('Degree','Closeness','PageRank','GO.BP_SemSim','GO.CC_SemSim','GO.MF_SemSim')
+  negcoexp_pval_vector[[i]]=vector(mode = 'list',length=6)
+  names(negcoexp_pval_vector[[i]])=c('Degree','Closeness','PageRank','GO.BP_SemSim','GO.CC_SemSim','GO.MF_SemSim')
+  
+}
+
+poscoexp_degree=negcoexp_degree=poscoexp_pgr=negcoexp_pgr=poscoexp_cls=negcoexp_cls=poscoexp_GO=negcoexp_GO=vector(mode = 'list',length = 2)
+names(poscoexp_degree)=names(negcoexp_degree)=names(poscoexp_pgr)=names(negcoexp_pgr)=names(poscoexp_cls)=names(negcoexp_cls)=names(poscoexp_GO)=names(negcoexp_GO)=c('OA_Healthy','OA_Disease')
+poscoexp_GO$OA_Healthy=poscoexp_GO$OA_Disease=negcoexp_GO$OA_Healthy=negcoexp_GO$OA_Disease=vector(mode = 'list',length=3)
+names(poscoexp_GO$OA_Healthy)=names(poscoexp_GO$OA_Disease)=names(negcoexp_GO$OA_Healthy)=names(negcoexp_GO$OA_Disease)=c('BP','CC','MF')
+
+hsGO=vector(mode = "list",length = 3)
+names(hsGO)=c('BP','CC','MF')
+hsGO$BP=godata(OrgDb='org.Hs.eg.db',keytype='SYMBOL',ont='BP')
+hsGO$CC=godata(OrgDb='org.Hs.eg.db',keytype='SYMBOL',ont='CC')
+hsGO$MF=godata(OrgDb='org.Hs.eg.db',keytype='SYMBOL',ont='MF')
+
+for (w in 1:length(pos_weight)){
+    
+      poscoexp_degree$OA_Healthy=degree(graph.data.frame(d=data.frame(ends(graph = OA_DCN$Healthy,es = which(E(OA_DCN$Healthy)$weight<pos_weight[w]),names = T)),directed = F))
+      poscoexp_degree$OA_Disease=degree(graph.data.frame(d=data.frame(ends(graph = OA_DCN$Diseased,es = which(E(OA_DCN$Diseased)$weight>pos_weight[w]),names = T)),directed = F))
+      negcoexp_degree$OA_Healthy=degree(graph.data.frame(d=data.frame(ends(graph = OA_DCN$Healthy,es = which(E(OA_DCN$Healthy)$weight<neg_weight[w]),names = T)),directed = F))
+      negcoexp_degree$OA_Disease=degree(graph.data.frame(d=data.frame(ends(graph = OA_DCN$Diseased,es = which(E(OA_DCN$Diseased)$weight>neg_weight[w]),names = T)),directed = F))
+      poscoexp_cls$OA_Healthy=closeness(graph.data.frame(d=data.frame(ends(graph = OA_DCN$Healthy,es = which(E(OA_DCN$Healthy)$weight<pos_weight[w]),names = T)),directed = F))
+      poscoexp_cls$OA_Disease=closeness(graph.data.frame(d=data.frame(ends(graph = OA_DCN$Diseased,es = which(E(OA_DCN$Diseased)$weight>pos_weight[w]),names = T)),directed = F))
+      negcoexp_cls$OA_Healthy=closeness(graph.data.frame(d=data.frame(ends(graph = OA_DCN$Healthy,es = which(E(OA_DCN$Healthy)$weight<neg_weight[w]),names = T)),directed = F))
+      negcoexp_cls$OA_Disease=closeness(graph.data.frame(d=data.frame(ends(graph = OA_DCN$Diseased,es = which(E(OA_DCN$Diseased)$weight>neg_weight[w]),names = T)),directed = F))
+      poscoexp_pgr$OA_Healthy=page_rank(graph.data.frame(d=data.frame(ends(graph = OA_DCN$Healthy,es = which(E(OA_DCN$Healthy)$weight<pos_weight[w]),names = T)),directed = F))$vector
+      poscoexp_pgr$OA_Disease=page_rank(graph.data.frame(d=data.frame(ends(graph = OA_DCN$Diseased,es = which(E(OA_DCN$Diseased)$weight>pos_weight[w]),names = T)),directed = F))$vector
+      negcoexp_pgr$OA_Healthy=page_rank(graph.data.frame(d=data.frame(ends(graph = OA_DCN$Healthy,es = which(E(OA_DCN$Healthy)$weight<neg_weight[w]),names = T)),directed = F))$vector
+      negcoexp_pgr$OA_Disease=page_rank(graph.data.frame(d=data.frame(ends(graph = OA_DCN$Diseased,es = which(E(OA_DCN$Diseased)$weight>neg_weight[w]),names = T)),directed = F))$vector
+      poscoexp_GO$OA_Healthy$BP=mgeneSim(genes = V(graph.data.frame(d=data.frame(ends(graph = OA_DCN$Healthy,es = which(E(OA_DCN$Healthy)$weight<pos_weight[w]),names = T)),directed = F))$name,semData = hsGO$BP,measure = 'Wang',drop = 'IEA',combine = 'BMA',verbose = T)
+      poscoexp_GO$OA_Disease$BP=mgeneSim(genes = V(graph.data.frame(d=data.frame(ends(graph = OA_DCN$Diseased,es = which(E(OA_DCN$Diseased)$weight>pos_weight[w]),names = T)),directed = F))$name,semData = hsGO$BP,measure = 'Wang',drop = 'IEA',combine = 'BMA',verbose = T)
+      negcoexp_GO$OA_Healthy$BP=mgeneSim(genes = V(graph.data.frame(d=data.frame(ends(graph = OA_DCN$Healthy,es = which(E(OA_DCN$Healthy)$weight<neg_weight[w]),names = T)),directed = F))$name,semData = hsGO$BP,measure = 'Wang',drop = 'IEA',combine = 'BMA',verbose = T)
+      negcoexp_GO$OA_Disease$BP=mgeneSim(genes = V(graph.data.frame(d=data.frame(ends(graph = OA_DCN$Diseased,es = which(E(OA_DCN$Diseased)$weight>neg_weight[w]),names = T)),directed = F))$name,semData = hsGO$BP,measure = 'Wang',drop = 'IEA',combine = 'BMA',verbose = T)
+      poscoexp_GO$OA_Healthy$CC=mgeneSim(genes = V(graph.data.frame(d=data.frame(ends(graph = OA_DCN$Healthy,es = which(E(OA_DCN$Healthy)$weight<pos_weight[w]),names = T)),directed = F))$name,semData = hsGO$CC,measure = 'Wang',drop = 'IEA',combine = 'BMA',verbose = T)
+      poscoexp_GO$OA_Disease$CC=mgeneSim(genes = V(graph.data.frame(d=data.frame(ends(graph = OA_DCN$Diseased,es = which(E(OA_DCN$Diseased)$weight>pos_weight[w]),names = T)),directed = F))$name,semData = hsGO$CC,measure = 'Wang',drop = 'IEA',combine = 'BMA',verbose = T)
+      negcoexp_GO$OA_Healthy$CC=mgeneSim(genes = V(graph.data.frame(d=data.frame(ends(graph = OA_DCN$Healthy,es = which(E(OA_DCN$Healthy)$weight<neg_weight[w]),names = T)),directed = F))$name,semData = hsGO$CC,measure = 'Wang',drop = 'IEA',combine = 'BMA',verbose = T)
+      negcoexp_GO$OA_Disease$CC=mgeneSim(genes = V(graph.data.frame(d=data.frame(ends(graph = OA_DCN$Diseased,es = which(E(OA_DCN$Diseased)$weight>neg_weight[w]),names = T)),directed = F))$name,semData = hsGO$CC,measure = 'Wang',drop = 'IEA',combine = 'BMA',verbose = T)
+      poscoexp_GO$OA_Healthy$MF=mgeneSim(genes = V(graph.data.frame(d=data.frame(ends(graph = OA_DCN$Healthy,es = which(E(OA_DCN$Healthy)$weight<pos_weight[w]),names = T)),directed = F))$name,semData = hsGO$MF,measure = 'Wang',drop = 'IEA',combine = 'BMA',verbose = T)
+      poscoexp_GO$OA_Disease$MF=mgeneSim(genes = V(graph.data.frame(d=data.frame(ends(graph = OA_DCN$Diseased,es = which(E(OA_DCN$Diseased)$weight>pos_weight[w]),names = T)),directed = F))$name,semData = hsGO$MF,measure = 'Wang',drop = 'IEA',combine = 'BMA',verbose = T)
+      negcoexp_GO$OA_Healthy$MF=mgeneSim(genes = V(graph.data.frame(d=data.frame(ends(graph = OA_DCN$Healthy,es = which(E(OA_DCN$Healthy)$weight<neg_weight[w]),names = T)),directed = F))$name,semData = hsGO$MF,measure = 'Wang',drop = 'IEA',combine = 'BMA',verbose = T)
+      negcoexp_GO$OA_Disease$MF=mgeneSim(genes = V(graph.data.frame(d=data.frame(ends(graph = OA_DCN$Diseased,es = which(E(OA_DCN$Diseased)$weight>neg_weight[w]),names = T)),directed = F))$name,semData = hsGO$MF,measure = 'Wang',drop = 'IEA',combine = 'BMA',verbose = T)
+      
+      poscoexp_pval_vector[[w]][[1]]=-log10(wilcox.test(x=poscoexp_degree$OA_Healthy,y=poscoexp_degree$OA_Disease)$p.value)
+      negcoexp_pval_vector[[w]][[1]]=-log10(wilcox.test(x=negcoexp_degree$OA_Healthy,y=negcoexp_degree$OA_Disease)$p.value)
+      poscoexp_pval_vector[[w]][[2]]=-log10(wilcox.test(x=poscoexp_pgr$OA_Healthy,y=poscoexp_pgr$OA_Disease)$p.value)
+      negcoexp_pval_vector[[w]][[2]]=-log10(wilcox.test(x=negcoexp_pgr$OA_Healthy,y=negcoexp_pgr$OA_Disease)$p.value)
+      poscoexp_pval_vector[[w]][[3]]=-log10(wilcox.test(x=poscoexp_cls$OA_Healthy,y=poscoexp_cls$OA_Disease)$p.value)
+      negcoexp_pval_vector[[w]][[3]]=-log10(wilcox.test(x=negcoexp_cls$OA_Healthy,y=negcoexp_cls$OA_Disease)$p.value)
+      poscoexp_pval_vector[[w]][[4]]=-log10(wilcox.test(x=poscoexp_GO$OA_Healthy$BP,y=poscoexp_GO$OA_Disease$BP)$p.value)
+      negcoexp_pval_vector[[w]][[4]]=-log10(wilcox.test(x=negcoexp_GO$OA_Healthy$BP,y=negcoexp_GO$OA_Disease$BP)$p.value)
+      poscoexp_pval_vector[[w]][[5]]=-log10(wilcox.test(x=poscoexp_GO$OA_Healthy$CC,y=poscoexp_GO$OA_Disease$CC)$p.value)
+      negcoexp_pval_vector[[w]][[5]]=-log10(wilcox.test(x=negcoexp_GO$OA_Healthy$CC,y=negcoexp_GO$OA_Disease$MF)$p.value)
+      poscoexp_pval_vector[[w]][[6]]=-log10(wilcox.test(x=poscoexp_GO$OA_Healthy$MF,y=poscoexp_GO$OA_Disease$MF)$p.value)
+      negcoexp_pval_vector[[w]][[6]]=-log10(wilcox.test(x=negcoexp_GO$OA_Healthy$MF,y=negcoexp_GO$OA_Disease$MF)$p.value)
+    
+}
+
