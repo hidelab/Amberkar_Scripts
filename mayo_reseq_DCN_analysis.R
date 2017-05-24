4210368dd4efa21087eab9320dd8447ad17cad3b
ssaamberkar <sandeep.amberkar18@gmail.com>
2017-05-24 17:50:06 +0100

    Restarted analysis at block 770

--- mayo_reseq_DCN_analysis.R
+++ mayo_reseq_DCN_analysis.R
@@ -74,7 +74,7 @@ for(t in 1:2){
   n.t<-ncol(t_exprs_rank)
   gene.names<-rownames(exprs_rank)
 
-  i<-503
+  i<-770
   start<-i*blocksize+1
   end<-min((i+1)*blocksize, number_of_combinations)
 
--- mayo_reseq_DCN_analysis.sh
+++ mayo_reseq_DCN_analysis.sh
@@ -7,6 +7,6 @@
 #$ -pe openmp 8 
 #Job Name 
 #-N MAYO_ReSeq_DCN
-module load module load apps/R/3.3.1
-module load module load compilers/gcc/6.2
+module load apps/R/3.3.1
+module load compilers/gcc/6.2
 R CMD BATCH --no-save --no-restore /shared/hidelab2/user/md4zsa/Work/Amberkar_Scripts/mayo_reseq_DCN_analysis.R
