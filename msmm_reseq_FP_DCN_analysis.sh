eed644e209bb88b1b81d04766b2f0bfa3836620e
ssaamberkar <sandeep.amberkar18@gmail.com>
2017-05-26 16:36:13 +0100

    fixed minor filename bugs

--- msmm_reseq_FP_DCN_analysis.sh
+++ msmm_reseq_FP_DCN_analysis.sh
@@ -11,4 +11,4 @@
 #-N "MSMM_ReSeq_FP_DCN"
 module load apps/R/3.3.1
 module load compilers/gcc/6.2
-R CMD BATCH --no-save --no-restore /shared/hidelab2/user/md4zsa/Work/Amberkar_Scripts/mssm_reseq_FP_DCN_analysis.R
+R CMD BATCH --no-save --no-restore /shared/hidelab2/user/md4zsa/Work/Amberkar_Scripts/msmm_reseq_FP_DCN_analysis.R
--- msmm_reseq_IFG_DCN_analysis.sh
+++ msmm_reseq_IFG_DCN_analysis.sh
@@ -6,7 +6,7 @@
 #Cores 
 #$ -pe openmp 8 
 #Job Name 
-#-N MSMM_ReSeq_IFG_DCN
+#-N "MSMM_ReSeq_IFG_DCN""
 module load apps/R/3.3.1
 module load compilers/gcc/6.2
-R CMD BATCH --no-save --no-restore /shared/hidelab2/user/md4zsa/Work/Amberkar_Scripts/mssm_reseq_IFG_DCN_analysis.R
+R CMD BATCH --no-save --no-restore /shared/hidelab2/user/md4zsa/Work/Amberkar_Scripts/msmm_reseq_IFG_DCN_analysis.R
