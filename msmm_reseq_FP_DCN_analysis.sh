6fbb038ef36c87aa5e23d9bda84c19213c62435e
ssaamberkar <sandeep.amberkar18@gmail.com>
2017-05-26 16:30:40 +0100

    Add to Hidelab queue

--- msmm_reseq_FP_DCN_analysis.sh
+++ msmm_reseq_FP_DCN_analysis.sh
@@ -5,8 +5,10 @@
 #-l rmem=12G 
 #Cores 
 #$ -pe openmp 8 
+#Queue
+# -P hidelab
 #Job Name 
-#-N MSMM_ReSeq_FP_DCN
+#-N "MSMM_ReSeq_FP_DCN"
 module load apps/R/3.3.1
 module load compilers/gcc/6.2
 R CMD BATCH --no-save --no-restore /shared/hidelab2/user/md4zsa/Work/Amberkar_Scripts/mssm_reseq_FP_DCN_analysis.R
