54529bc83c35c92bea028078e6d3435ca377c558
ssaamberkar <sandeep.amberkar18@gmail.com>
2017-05-26 16:13:59 +0100

    Modified for removal of NA count genes

---
+++ msmm_reseq_PHG_DCN_analysis.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+#Job time 
+#-l h_rt=120:00:00 
+#Request resources 
+#-l rmem=12G 
+#Cores 
+#$ -pe openmp 8 
+#Job Name 
+#-N MSMM_ReSeq_PHG_DCN
+module load apps/R/3.3.1
+module load compilers/gcc/6.2
+R CMD BATCH --no-save --no-restore /shared/hidelab2/user/md4zsa/Work/Amberkar_Scripts/mssm_reseq_PHG_DCN_analysis.R
