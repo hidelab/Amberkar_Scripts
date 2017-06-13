27306f597a0c168ed652a8262d7cc2e9b6be5489
ssaamberkar <sandeep.amberkar18@gmail.com>
2017-06-13 14:57:16 +0100

    hidelab queue removed

--- mayo_reseq_DCN_analysis.sh
+++ mayo_reseq_DCN_analysis.sh
@@ -7,8 +7,7 @@
 #$ -pe openmp 8 
 #Job Name 
 #-N MAYO_ReSeq_DCN
-#Queue
-#-P hidelab
+
 module load apps/R/3.3.1
 module load compilers/gcc/6.2
 R CMD BATCH --no-save --no-restore /shared/hidelab2/user/md4zsa/Work/Amberkar_Scripts/mayo_reseq_DCN_analysis.R
