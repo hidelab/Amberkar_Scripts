8ffc3d6db5e2abc004abe2a76d2eddf56e7cfca9
ssaamberkar <sandeep.amberkar18@gmail.com>
2017-06-13 12:22:50 +0100

    hidelab queue added

--- mayo_reseq_DCN_analysis.sh
+++ mayo_reseq_DCN_analysis.sh
@@ -4,9 +4,11 @@
 #Request resources 
 #-l rmem=12G 
 #Cores 
-#$ -pe openmp 12 
+#$ -pe openmp 8 
 #Job Name 
 #-N MAYO_ReSeq_DCN
+#Queue
+#-P hidelab
 module load apps/R/3.3.1
 module load compilers/gcc/6.2
 R CMD BATCH --no-save --no-restore /shared/hidelab2/user/md4zsa/Work/Amberkar_Scripts/mayo_reseq_DCN_analysis.R
