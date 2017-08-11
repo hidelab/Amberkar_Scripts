01c5655da9303662eda9f829e7f65ed5ae8d302f
ssaamberkar <sandeep.amberkar18@gmail.com>
2017-08-11 14:58:42 +0100

    restarted at block 1183

--- ipah_pilot_DCN_analysis.R
+++ ipah_pilot_DCN_analysis.R
@@ -101,8 +101,8 @@ gene.names=rownames(ipah_counts.filtered2)
 number_of_combinations=choose(length(gene.names),2)
 dir.create("cocor_results",showWarnings = T,mode = "0777")
 
-i=1182
-blocksize=100000
+i=1183
+blocksize=1000
 start<-i*blocksize+1
 end<-min((i+1)*blocksize, number_of_combinations)
 
