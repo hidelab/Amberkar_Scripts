63c52a0b336701f9a102d484e4ec33f1ae47efef
ssaamberkar <sandeep.amberkar18@gmail.com>
2017-08-24 17:02:18 +0100

    shell script for 150bp wrapper script

---
+++ ipah_pilot_150bp_DCN_analysis.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+#Job time 
+#-l h_rt=120:00:00 
+#Request resources 
+#-l mem=8G 
+#Cores 
+#$-pe openmp 12 
+#Job Name 
+#-N IPAH_DCN2
+
+module load mpi/openmpi/2.1.1/gcc-4.8.5
+module load apps/R/3.4.0/gcc-4.8.5
+R CMD BATCH --no-save --no-restore /shared/hidelab2/user/md4zsa/Work/Amberkar_Scripts/ipah_pilot_DCN_analysis.R
