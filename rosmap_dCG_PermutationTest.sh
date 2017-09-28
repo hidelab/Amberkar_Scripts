8dce2f90d3e33dd054d40be951a83fd3cfb15f25
ssaamberkar <sandeep.amberkar18@gmail.com>
2017-09-28 20:25:46 +0100

    resubmit to hidelab queue

--- rosmap_dCG_PermutationTest.sh
+++ rosmap_dCG_PermutationTest.sh
@@ -8,8 +8,8 @@
 #Job Name 
 #$-N ROSMAP_dCP
 #Job queue
-#$-P rse
+#-P hidelab
 
 module load compilers/gcc/6.2
-module load apps/R/3.3.1/gcc-6.2
+module load apps/R/3.3.1
 R CMD BATCH --no-save --no-restore /shared/hidelab2/user/md4zsa/Work/Amberkar_Scripts/rosmap_dCG_PermutationTest.R
