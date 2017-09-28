1904ebebfb611280381c8c2d2997c777c4b9c9f4
ssaamberkar <sandeep.amberkar18@gmail.com>
2017-09-28 15:04:20 +0100

    shell script for dCP analysis, added to RSE queue

--- rosmap_dCG_PermutationTest.sh
+++ rosmap_dCG_PermutationTest.sh
@@ -1,14 +1,14 @@
 #!/bin/bash 
 #Job time 
-#-l h_rt=240:00:00 
+#$-l h_rt=240:00:00 
 #Request resources 
-#-l mem=12G 
+#$-l mem=12G 
 #Cores 
 #$-pe openmp 12
 #Job Name 
-#-N ROSMAP_dCP
+#$-N ROSMAP_dCP
 #Job queue
-#-P rse
+#$-P rse
 
 module load mpi/openmpi/2.1.1/gcc-4.8.5
 module load apps/R/3.4.0/gcc-4.8.5
