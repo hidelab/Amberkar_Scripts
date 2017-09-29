f2e77158178c5dbdb0dae40af423f678da508db2
ssaamberkar <sandeep.amberkar18@gmail.com>
2017-09-29 16:20:52 +0100

    R scripts for DCL analysis

---
+++ rosmap_DCL_analysis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+#Job time 
+#-l h_rt=96:00:00 
+#Request resources 
+#-l rmem=12G 
+#Cores 
+#$-pe openmp 12 
+#Job Name 
+#$-N ROSMAP_DCe
+#Queue
+#$-P hidelab
+
+module load apps/R/3.3.1
+module load compilers/gcc/6.2
+R CMD BATCH --no-save --no-restore /shared/hidelab2/user/md4zsa/Work/Amberkar_Scripts/rosmap_DCL_analysis.R
