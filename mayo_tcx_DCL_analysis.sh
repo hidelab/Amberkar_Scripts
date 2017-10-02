0aee7175d70f9aefaa71b3f8504c2cff3049dd00
ssaamberkar <sandeep.amberkar18@gmail.com>
2017-10-02 10:23:19 +0100

    submit to Sharc

--- mayo_tcx_DCL_analysis.sh
+++ mayo_tcx_DCL_analysis.sh
@@ -7,7 +7,9 @@
 #$ -pe openmp 12 
 #Job Name 
 #$ -N MAYO_TCX_DCe
+#Queue
+#$ -P rse
 
-module load apps/R/3.3.1
-module load compilers/gcc/6.2
+module load apps/R/3.4.0/gcc-4.8.5
+module load mpi/openmpi/2.1.1/gcc-4.8.5
 R CMD BATCH --no-save --no-restore /shared/hidelab2/user/md4zsa/Work/Amberkar_Scripts/mayo_tcx_DCL_analysis.R
