5d11d04b3885a724f24de1bed6d061d04e0b0f47
ssaamberkar <sandeep.amberkar18@gmail.com>
2017-10-02 10:32:38 +0100

    submit to Sharc

--- mayo_tcx_DCL_analysis.sh
+++ mayo_tcx_DCL_analysis.sh
@@ -1,14 +1,14 @@
 #!/bin/bash 
 #Job time 
-#$ -l h_rt=120:00:00 
+#$-l h_rt=120:00:00 
 #Request resources 
-#$ -l rmem=12G 
+#$-l rmem=12G 
 #Cores 
-#$ -pe openmp 12 
+#$-pe openmp 12 
 #Job Name 
-#$ -N MAYO_TCX_DCe
+#$-N MAYO_TCX_DCe
 #Queue
-#$ -P rse
+#$-P rse
 
 module load apps/R/3.4.0/gcc-4.8.5
 module load mpi/openmpi/2.1.1/gcc-4.8.5
