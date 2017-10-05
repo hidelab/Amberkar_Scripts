0ae288a1837d86e95790583185cbe189cc36d915
ssaamberkar <sandeep.amberkar18@gmail.com>
2017-10-05 20:01:21 +0100

    resubmit to hidelab queue

--- mayo_cer_DCL_analysis.sh
+++ mayo_cer_DCL_analysis.sh
@@ -8,8 +8,8 @@
 #Job Name 
 #$-N MAYO_CER_DCe
 #Queue
-#$-P rse
+#$-P hidelab
 
-module load apps/R/3.4.0/gcc-4.8.5
-module load mpi/openmpi/2.1.1/gcc-4.8.5
+module load apps/R/3.3.1
+module load compilers/gcc/6.2
 R CMD BATCH --no-save --no-restore /shared/hidelab2/user/md4zsa/Work/Amberkar_Scripts/mayo_cer_DCL_analysis.R
