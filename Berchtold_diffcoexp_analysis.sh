67c2f407f830546c0da03a20d0fb9525ffd5db73
ssaamberkar <sandeep.amberkar18@gmail.com>
2018-03-12 17:45:12 +0000

    resubmit in general queue

--- Berchtold_diffcoexp_analysis.sh
+++ Berchtold_diffcoexp_analysis.sh
@@ -3,12 +3,11 @@
 #$-l h_rt=12:00:00 
 #Request resources 
 #$-l mem=64G
-#$-l rmem=32G
+#$-l rmem=64G
 #$-pe openmp 4
 #Job Name 
 #$-N Berchtold_diffcoep
-#Queue
-#-P hidelab
+
 
 module load apps/R/3.3.1
 module load compilers/gcc/6.2
