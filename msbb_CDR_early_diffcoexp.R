61e023656e0657289cba248b70acffe3c1efe612
ssaamberkar <sandeep.amberkar18@gmail.com>
2018-05-07 00:22:22 +0100

    early diffcoexp CDR analysis - full

--- msbb_CDR_early_diffcoexp.R
+++ msbb_CDR_early_diffcoexp.R
@@ -9,7 +9,8 @@ load("msbb_gse84422_Lobe_DEG_DEP.RData")
 for(i in 1:length(msbb_gse84422_GPL96_97_byRegion.exprs)){
   c_exprs=msbb_gse84422_GPL96_97_byRegion.exprs[[i]][,msbb_gse84422.pData$GPL96$pseudoSampleID[which(msbb_gse84422.pData$GPL96$`brain region:ch1`==names(msbb_gse84422_GPL96_97_byRegion.exprs)[i]&msbb_gse84422.pData$GPL96$SampleTypeCDR=="CDR0")]]
   d_exprs=msbb_gse84422_GPL96_97_byRegion.exprs[[i]][,msbb_gse84422.pData$GPL96$pseudoSampleID[which(msbb_gse84422.pData$GPL96$`brain region:ch1`==names(msbb_gse84422_GPL96_97_byRegion.exprs)[i]&msbb_gse84422.pData$GPL96$SampleTypeCDR=="CDR1")]]
-  diffcoexp_out=diffcoexp(exprs.1 = c_exprs[1:500,],exprs.2 = d_exprs[1:500,],r.method="spearman",rth=0.6,q.diffth=0.1,q.dcgth=0.1)
+  diffcoexp_out=diffcoexp(exprs.1 = c_exprs[,],exprs.2 = d_exprs[,],r.method="spearman",rth=0.6,q.diffth=0.1,q.dcgth=0.1)
   fn_prefix=gsub(pattern = " ",replacement = "_",names(msbb_gse84422_GPL96_97_byRegion.exprs)[i])
   saveRDS(diffcoexp_out,paste(fn_prefix,"diffcoexp_CDR0_CDR1.RDS",sep="_"))
+  proc.time()
 }
