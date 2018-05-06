b339deb6bfdb999b496235f9534645d7d9499e67
ssaamberkar <sandeep.amberkar18@gmail.com>
2018-05-07 00:17:53 +0100

    early diffcoexp CDR analysis

---
+++ msbb_CDR_early_diffcoexp.R
@@ -0,0 +1,15 @@
+library(org.Hs.eg.db)
+library(dplyr)
+library(magrittr)
+library(data.table)
+library(diffcoexp)
+
+setwd("/shared/hidelab2/user/md4zsa/Work/Data/MSBB_Array19/GSE84422/")
+load("msbb_gse84422_Lobe_DEG_DEP.RData")
+for(i in 1:length(msbb_gse84422_GPL96_97_byRegion.exprs)){
+  c_exprs=msbb_gse84422_GPL96_97_byRegion.exprs[[i]][,msbb_gse84422.pData$GPL96$pseudoSampleID[which(msbb_gse84422.pData$GPL96$`brain region:ch1`==names(msbb_gse84422_GPL96_97_byRegion.exprs)[i]&msbb_gse84422.pData$GPL96$SampleTypeCDR=="CDR0")]]
+  d_exprs=msbb_gse84422_GPL96_97_byRegion.exprs[[i]][,msbb_gse84422.pData$GPL96$pseudoSampleID[which(msbb_gse84422.pData$GPL96$`brain region:ch1`==names(msbb_gse84422_GPL96_97_byRegion.exprs)[i]&msbb_gse84422.pData$GPL96$SampleTypeCDR=="CDR1")]]
+  diffcoexp_out=diffcoexp(exprs.1 = c_exprs[1:500,],exprs.2 = d_exprs[1:500,],r.method="spearman",rth=0.6,q.diffth=0.1,q.dcgth=0.1)
+  fn_prefix=gsub(pattern = " ",replacement = "_",names(msbb_gse84422_GPL96_97_byRegion.exprs)[i])
+  saveRDS(diffcoexp_out,paste(fn_prefix,"diffcoexp_CDR0_CDR1.RDS",sep="_"))
+}
---
+++ msbb_CDR_early_diffcoexp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+#Job time 
+#$-l h_rt=24:00:00 
+#Request resources 
+#$-l mem=128G
+#$-l rmem=96G
+#Job Name 
+#$-N MSBB_EarlyDCN
+#Queue
+#$-P hidelab
+
+
+module load apps/R/3.3.1
+module load compilers/gcc/6.2
+R CMD BATCH --no-save --no-restore /shared/hidelab2/user/md4zsa/Work/Amberkar_Scripts/msbb_CDR_early_diffcoexp.R
