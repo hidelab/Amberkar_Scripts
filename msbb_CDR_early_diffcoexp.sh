f6654163ed3bf41c184d150c0063e0c26299e21c
ssaamberkar <sandeep.amberkar18@gmail.com>
2018-05-16 15:33:46 +0100

    submit to Shark

--- msbb_CDR_early_diffcoexp.sh
+++ msbb_CDR_early_diffcoexp.sh
@@ -3,13 +3,12 @@
 #$-l h_rt=12:00:00 
 #Request resources 
 #$-l mem=128G
-#$-l rmem=72G
+#$-l rmem=128G
 #Job Name 
 #$-N MSBB_EarlyDCN
 #Queue
-#$-P hidelab
+#$-P RSE
 
-
-module load apps/R/3.3.1
-module load compilers/gcc/6.2
+module load apps/R/3.4.0/gcc-4.8.5
+module load mpi/openmpi/2.1.1/gcc-6.2
 R CMD BATCH --no-save --no-restore /shared/hidelab2/user/md4zsa/Work/Amberkar_Scripts/msbb_CDR_early_diffcoexp.R
