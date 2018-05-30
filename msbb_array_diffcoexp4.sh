f9300eb4a620ba0c58e8c5e52d7da1f751da278d
ssaamberkar <sandeep.amberkar18@gmail.com>
2018-05-30 23:04:16 +0100

    early diffcoexp AD

---
+++ msbb_array_diffcoexp4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+#Job time 
+#$-l h_rt=36:00:00 
+#Request resources 
+#$-l mem=256G
+#$-l rmem=128G
+#Job Name 
+#$-N Early_Diffcoexp
+
+module load apps/R/3.3.1
+module compilers/gcc/6.2
+R CMD BATCH --no-save --no-restore /shared/hidelab2/user/md4zsa/Work/Amberkar_Scripts/msbb_array_diffcoexp4.R
