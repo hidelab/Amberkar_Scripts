928632c2a9bea26714a2b7b59e1dc94c748ba521
ssaamberkar <sandeep.amberkar18@gmail.com>
2018-06-11 15:46:54 +0100

    modified for full analysis

--- msbb_array_diffcoexp4_TED_analysis.R
+++ msbb_array_diffcoexp4_TED_analysis.R
@@ -22,40 +22,44 @@ earlyAD_DCLs=lapply(earlyAD_diffcoexp_results,function(x)x$DCLs)
 
 earlyAD_DRrank.TED=earlyAD_DRrank.TED=vector(mode = "list",length = length(earlyAD_samples.exprs))
 names(earlyAD_DRrank.TED)=names(earlyAD_DRrank.TED)=names(earlyAD_diffcoexp_results)
-genes=earlyAD_DCGs$Frontal_Pole
-dcls=earlyAD_DCLs$Frontal_Pole
-prob<-nrow(dcls)/choose(nrow(earlyAD_DCGs$Frontal_Pole), 2)
-
-ProcessElement<-function(ic) {
-  regulator_gene_symbol<-names(humanRegnetwork)[ic]
-  targets<-humanRegnetwork[[ic]]
-  common.genes<-intersect(targets, genes)
-  n<-length(common.genes)
-  N<-n * (n-1)/2
-  rowIntersect<-function(x) {
-    length(intersect(x, common.genes))
+compute_time=list()
+for(t in 1:length(earlyAD_diffcoexp_results)){
+  genes=earlyAD_DCGs[[t]]
+  dcls=earlyAD_DCLs[[t]]
+  prob<-nrow(dcls)/choose(nrow(earlyAD_DCGs[[t]]), 2)
+  
+  ProcessElement<-function(ic) {
+    regulator_gene_symbol<-names(humanRegnetwork)[ic]
+    targets<-humanRegnetwork[[ic]]
+    common.genes<-intersect(targets, genes)
+    n<-length(common.genes)
+    N<-n * (n-1)/2
+    rowIntersect<-function(x) {
+      length(intersect(x, common.genes))
+    }
+    y<-apply(dcls, 1, rowIntersect)
+    e<-sum(y==2)
+    #binom.test(e, N, prob, alternative="greater")
+    p.value <- pbinom(e-1, N, prob, lower.tail = FALSE, log.p = FALSE)
+    tmp <- data.frame(regulator_gene_symbol = regulator_gene_symbol,
+                      n.target.gene.in.data = n,
+                      n.target.pairs = e,
+                      n.expected.target.pairs = N * prob,
+                      p.value = p.value
+    )
+    setTxtProgressBar(pb,ic)
+    return(tmp)
   }
-  y<-apply(dcls, 1, rowIntersect)
-  e<-sum(y==2)
-  #binom.test(e, N, prob, alternative="greater")
-  p.value <- pbinom(e-1, N, prob, lower.tail = FALSE, log.p = FALSE)
-  tmp <- data.frame(regulator_gene_symbol = regulator_gene_symbol,
-                    n.target.gene.in.data = n,
-                    n.target.pairs = e,
-                    n.expected.target.pairs = N * prob,
-                    p.value = p.value
-  )
-  setTxtProgressBar(pb,ic)
-  return(tmp)
+  nc = 4
+  input = 1:length(humanRegnetwork)
+  pb = txtProgressBar(min=0,max=length(humanRegnetwork), style=3, initial=0)
+  cat("\n")
+  result<- mclapply(input, ProcessElement, mc.cores=nc)
+  close(pb)
+  library(data.table)
+  result<-rbindlist(result)
+  result<-as.data.frame(result)
+  result$p.adjusted<-p.adjust(result$p.value)
+  saveRDS(result,paste("earlyAD",names(earlyAD_diffcoexp_results)[t],"TED_new_pbinom_out.RDS",sep = "_"))
+  compute_time[[t]]=proc.time()
 }
-nc = 4
-input = 1:length(humanRegnetwork)
-pb = txtProgressBar(min=0,max=length(humanRegnetwork), style=3, initial=0)
-cat("\n")
-result<- mclapply(input, ProcessElement, mc.cores=nc)
-close(pb)
-library(data.table)
-result<-rbindlist(result)
-result<-as.data.frame(result)
-result$p.adjusted<-p.adjust(result$p.value)
-saveRDS(result,"earlyAD_TED_new_pbinom_out.RDS")
