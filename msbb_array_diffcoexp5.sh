1800cc249f8fb939c01fe26cd7bc3fa8bb704d2d
ssaamberkar <sandeep.amberkar18@gmail.com>
2018-06-27 12:43:44 +0100

    lateAD diffcoexp

---
+++ msbb_array_diffcoexp5.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+#Job time 
+#$-l h_rt=24:00:00 
+#Request resources 
+#$-l mem=128G
+#$-l rmem=128G
+#Job Name 
+#$-N Late_DCN
+#Queue
+#$-P rse
+
+module load apps/R/3.4.0/gcc-4.8.5
+module load mpi/openmpi/2.1.1/gcc-6.2
+R CMD BATCH --no-save --no-restore /shared/hidelab2/user/md4zsa/Work/Amberkar_Scripts/msbb_array_diffcoexp5.R
