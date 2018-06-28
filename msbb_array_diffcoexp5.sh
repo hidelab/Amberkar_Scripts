739cf70fd480f6d2096f80d856d5459f3fa0cf41
ssaamberkar <sandeep.amberkar18@gmail.com>
2018-06-28 10:42:23 +0100

    lateAD diffcoexp, bug fixes

--- msbb_array_diffcoexp5.sh
+++ msbb_array_diffcoexp5.sh
@@ -8,6 +8,10 @@
 #$-N Late_DCN
 #Queue
 #$-P rse
+#$ -m bea # send mails at beginning, end and if aborted unexpectedly
+#$ -M s.amberkar@Sheffield.ac.uk # mail sent to this address
+#$ -o LateAD_DCN.log # output file
+#$ -j y # send output and error to same file
 
 module load apps/R/3.4.0/gcc-4.8.5
 module load mpi/openmpi/2.1.1/gcc-6.2
