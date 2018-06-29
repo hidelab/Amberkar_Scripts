6a43cad01d603a97ddfb36b11f0db9c34b387ec2
ssaamberkar <sandeep.amberkar18@gmail.com>
2018-06-29 19:11:09 +0100

    TED analysis with new results

--- msbb_array_diffcoexp5_TED_analysis.sh
+++ msbb_array_diffcoexp5_TED_analysis.sh
@@ -5,14 +5,14 @@
 #$ -l rmem=16G
 #$ -pe smp 8
 #Job Name 
-#$-N Late_DCN
+#$-N LateAD_TED
 #Queue
 #$-P rse
 #$ -m bea # send mails at beginning, end and if aborted unexpectedly
 #$ -M s.amberkar@Sheffield.ac.uk # mail sent to this address
-#$ -o LateAD_DCN.log # output file
+#$ -o LateAD_TED.log # output file
 #$ -j y # send output and error to same file
 
 module load apps/R/3.4.0/gcc-4.8.5
 module load mpi/openmpi/2.1.1/gcc-6.2
-R CMD BATCH --no-save --no-restore /shared/hidelab2/user/md4zsa/Work/Amberkar_Scripts/msbb_array_diffcoexp5.R
+R CMD BATCH --no-save --no-restore /shared/hidelab2/user/md4zsa/Work/Amberkar_Scripts/msbb_array_diffcoexp5_TED_analysis.R
