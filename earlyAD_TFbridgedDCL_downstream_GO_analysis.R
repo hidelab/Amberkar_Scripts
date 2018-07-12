5178f3606117222811d6bb691a931bc8fe20fb5a
ssaamberkar <sandeep.amberkar18@gmail.com>
2018-07-12 23:15:25 +0100

    earlyAD TFbridgedDCL downtream GO simplification

--- earlyAD_TFbridgedDCL_downstream_GO_analysis.R
+++ earlyAD_TFbridgedDCL_downstream_GO_analysis.R
@@ -2,10 +2,12 @@ library(clusterProfiler)
 library(dplyr)
 library(org.Hs.eg.db)
 
+
 earlyAD_TF_bridged_DCL.TF_downstream=earlyAD_TF_bridged_DCL.TF_downstream_subgraph=earlyAD_TF_bridged_DCL.TF_downstream_subgraph_entrez=earlyAD_TF_bridged_DCL.TF_downstream_subgraph_GO=earlyAD_TF_bridged_DCL.TF_downstream_subgraph_GO_simplify=earlyAD_TF_bridged_DCL.TF_downstream_GOsummaries=vector(mode = "list",length = dim(common_earlyAD_sigTF_regulated_bridgedDCLs.df)[2])
 names(earlyAD_TF_bridged_DCL.TF_downstream)=names(earlyAD_TF_bridged_DCL.TF_downstream_subgraph)=names(earlyAD_TF_bridged_DCL.TF_downstream_subgraph_entrez)=names(earlyAD_TF_bridged_DCL.TF_downstream_subgraph_GO)=names(earlyAD_TF_bridged_DCL.TF_downstream_subgraph_GO_simplify)=names(earlyAD_TF_bridged_DCL.TF_downstream_GOsummaries)=colnames(common_earlyAD_sigTF_regulated_bridgedDCLs.df)
 
 setwd("/shared/hidelab2/user/md4zsa/Work/Data/MSBB_Array19/GSE84422/EarlyAD_diffcoexp/")
+common_earlyAD_sigTF_regulated_bridgedDCLs.df=read.table("common_earlyAD_sigTF_regulated_bridgedDCLs_df.txt")
 earlyAD_TF_bridged_DCL.TF_downstream=readRDS("earlyAD_TF_bridged_DCL_TF_downstream.RDS")
 earlyAD_TF_bridged_DCL.TF_downstream_subgraph=readRDS("earlyAD_TF_bridged_DCL_TF_downstream_subgraph.RDS")
 earlyAD_TF_bridged_DCL.TF_downstream_subgraph_entrez=readRDS("earlyAD_TF_bridged_DCL_TF_downstream_subgraph_entrez.RDS")
