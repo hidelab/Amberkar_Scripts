d5384fc6e08d306afe23886dfcc460cc63c015b4
ssaamberkar <sandeep.amberkar18@gmail.com>
2018-08-29 21:57:03 +0100

    ROSMAP DCL analysis

--- rosmap_earlyAD_diffcoexp_analysis.R
+++ rosmap_earlyAD_diffcoexp_analysis.R
@@ -3,8 +3,10 @@ library(synapseClient)
 library(data.table)
 library(org.Hs.eg.db)
 library(dplyr)
+library(diffcoexp)
 library(magrittr)
 library(devtools)
+
 synapseLogin()
 
 #Set working directory
@@ -16,10 +18,11 @@ rosmap_reseq_data=fread(rosmap_reseq_data_pointer@filePath,sep = "\t",header = T
 rosmap_covariates=read.table("ROSMAP_DLPFC_Covariates.tsv",header = T,sep = "\t",stringsAsFactors = F)
 
 #Collapse Ensembl IDs to gene symbols. For multiple Ensembl IDs for same gene, compute average expression
-rosmap_reseq_data$gene_symbol=unname(mapIds(x = EnsDb.Hsapiens.v79,keys=rosmap_reseq_data$ensembl_gene_id,keytype = "GENEID",column = "SYMBOL",multiVals = "first"))
-rosmap_reseq_data.agg=aggregate.data.frame(x=rosmap_reseq_data[,-c('ensembl_gene_id','gene_symbol')],by=list(Symbol=rosmap_reseq_data$gene_symbol),mean)
-rownames(rosmap_reseq_data.agg)=rosmap_reseq_data.agg$Symbol
-rosmap_reseq_data.agg=rosmap_reseq_data.agg[,-1]
+# rosmap_reseq_data$gene_symbol=unname(mapIds(x = EnsDb.Hsapiens.v79,keys=rosmap_reseq_data$ensembl_gene_id,keytype = "GENEID",column = "SYMBOL",multiVals = "first"))
+# rosmap_reseq_data.agg=aggregate.data.frame(x=rosmap_reseq_data[,-c('ensembl_gene_id','gene_symbol')],by=list(Symbol=rosmap_reseq_data$gene_symbol),mean)
+# rownames(rosmap_reseq_data.agg)=rosmap_reseq_data.agg$Symbol
+# rosmap_reseq_data.agg=rosmap_reseq_data.agg[,-1]
+rosmap_reseq_data.agg=readRDS("rosmap_reseq_data_agg.RDS")
 
 rosmap_covariates$SampleType="OTHER"
 rosmap_covariates$SampleType[rosmap_covariates$cogdx==1]="CONTROL"
@@ -31,7 +34,7 @@ rownames(rosmap_coldata)=colnames(rosmap_reseq_data.agg)
 
 rosmap_control.counts=rosmap_reseq_data.agg[,rosmap_covariates%>%dplyr::filter(SampleType=="CONTROL")%>%dplyr::pull(SampleID)]
 rosmap_cogdx2.counts=rosmap_reseq_data.agg[,rosmap_covariates%>%dplyr::filter(SampleType=="COGDX2")%>%dplyr::pull(SampleID)]
-rosmap_diffcoexp=diffcoexp(exprs.1 = rosmap_control.counts,exprs.2 = rosmap_cogdx2.counts,r.method="spearman",rth=0.6,q.diffth=0.1,q.dcgth=0.1)
+rosmap_diffcoexp=diffcoexp(exprs.1 = rosmap_control.counts[1:1000,],exprs.2 = rosmap_cogdx2.counts[1:1000,],r.method="spearman",rth=0.6,q.diffth=0.1,q.dcgth=0.1)
 saveRDS(rosmap_diffcoexp,"ROSMAP_earlyAD_diffcoexp_results.RDS")
 
 
