02a9cf78748ce45427c9a2745f78f98db9c42075
ssaamberkar <sandeep.amberkar18@gmail.com>
2018-11-08 00:14:16 +0000

    calculate mean gene expression

--- rosmap_AD_diffcoexp_analysis.R
+++ rosmap_AD_diffcoexp_analysis.R
@@ -39,4 +39,5 @@ rosmap_reseq_data2=readRDS("rosmap_reseq_data_agg.RDS")
 c_rosmap_exprs=rosmap_reseq_data2[,rosmap_covariates.df%>%dplyr::filter(Diagnosis=="CONTROL")%>%pull(SampleID)]
 t_rosmap_exprs=rosmap_reseq_data2[,rosmap_covariates.df%>%dplyr::filter(Diagnosis=="AD")%>%pull(SampleID)]
 rosmap.diffcoexp=diffcoexp(exprs.1=c_rosmap_exprs,exprs.2=t_rosmap_exprs,r.method="spearman",rth=0.6,q.diffth=0.1,q.dcgth=0.1)
+
 saveRDS(rosmap.diffcoexp,"rosmap_diffcoexp_CTRL_AD_out.RDS")
