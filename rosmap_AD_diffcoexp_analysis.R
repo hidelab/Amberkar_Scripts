da500389ffa14bd0dae5f5aa31b0703ba65a50a4
ssaamberkar <sandeep.amberkar18@gmail.com>
2018-11-08 11:29:32 +0000

    calculate mean gene expression

--- rosmap_AD_diffcoexp_analysis.R
+++ rosmap_AD_diffcoexp_analysis.R
@@ -30,13 +30,13 @@ setwd("/shared/hidelab2/user/md4zsa/Work/Data/AMP-AD_RNAseq_ReSeq/Normalised_cov
 # rosmap_reseq_data2=rosmap_reseq_data.agg[,-1]
 
 #Read ROSMAP covariates
-rosmap_covariates=synGet("syn3191087")
-rosmap_covariates.df=fread(rosmap_covariates@filePath,sep = ",",header = T,stringsAsFactors = F,showProgress = T,data.table = F)
-rosmap_covariates.df=rosmap_covariates.df%>%mutate(diagnosis=if_else((cogdx=4 & braaksc>=4 & ceradsc <= 2),true = "AD",false = "OTHER"))
-rosmap_covariates.df=rosmap_covariates.df%>%mutate(diagnosis=if_else((cogdx=1 & braaksc<=3 & ceradsc >= 3),true = "CONTROL",false = "OTHER"))
+rosmap_covariates=synGet("syn11024258")
+rosmap_covariates.df=fread(rosmap_covariates@filePath,sep = "\t",header = T,stringsAsFactors = F,showProgress = T,data.table = F)
+# rosmap_covariates.df=rosmap_covariates.df%>%mutate(diagnosis=if_else((cogdx=4 & braaksc>=4 & ceradsc <= 2),true = "AD",false = "OTHER"))
+# rosmap_covariates.df=rosmap_covariates.df%>%mutate(diagnosis=if_else((cogdx=1 & braaksc<=3 & ceradsc >= 3),true = "CONTROL",false = "OTHER"))
 
 rosmap_reseq_data2=readRDS("rosmap_reseq_data_agg.RDS")
-c_rosmap_exprs=rosmap_reseq_data2[,rosmap_covariates.df%>%dplyr::filter(diagnosis=="CONTROL")%>%pull(SampleID)]
-t_rosmap_exprs=rosmap_reseq_data2[,rosmap_covariates.df%>%dplyr::filter(diagnosis=="AD")%>%pull(SampleID)]
+c_rosmap_exprs=rosmap_reseq_data2[,rosmap_covariates.df%>%dplyr::filter(Diagnosis=="CONTROL")%>%pull(SampleID)]
+t_rosmap_exprs=rosmap_reseq_data2[,rosmap_covariates.df%>%dplyr::filter(Diagnosis=="AD")%>%pull(SampleID)]
 rosmap.diffcoexp=diffcoexp(exprs.1=c_rosmap_exprs,exprs.2=t_rosmap_exprs,r.method="spearman",rth=0.6,q.diffth=0.1,q.dcgth=0.1)
 saveRDS(rosmap.diffcoexp,"rosmap_diffcoexp_CTRL_AD_out.RDS")
